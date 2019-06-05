ecea6156cebbe67f1f42da1e7aa1282c2c87b7d3
Kevin Baird <kevincbaird@gmail.com>
2019-06-04 19:44:11 -0700

    Add triple and quadruple cases

--- test/ricardo_test.erl
+++ test/ricardo_test.erl
@@ -14,7 +14,9 @@ ricardo_test_() ->
             [
                 fun ricardo_empty_case/0,
                 fun ricardo_single_case/0,
-                fun ricardo_england_portugal_case/0
+                fun ricardo_england_portugal_case/0,
+                fun triple_case/0,
+                fun quadruple_case/0
             ]
     }.
 
@@ -31,7 +33,28 @@ ricardo_england_portugal_case() ->
     Eng = nation:make(<<"England">>, [{<<"cloth">>, 100}, {<<"wine">>, 120}]),
     Por = nation:make(<<"Portugal">>, [{<<"cloth">>, 90}, {<<"wine">>, 80}]),
     Results = ricardo:optimize([Eng, Por]),
-    ?assertEqual(#{<<"England">> => [<<"cloth">>], <<"Portugal">> => [<<"wine">>]}, Results).
+    ?assertEqual(#{<<"England">>  => [<<"cloth">>],
+                   <<"Portugal">> => [<<"wine">>]}, Results).
+
+triple_case() ->
+    Eng = nation:make(<<"England">>, [{<<"beer">>, 110}, {<<"cloth">>, 100}, {<<"wine">>, 120}]),
+    Ger = nation:make(<<"Germany">>, [{<<"beer">>, 50}, {<<"cloth">>, 80}, {<<"wine">>, 90}]),
+    Por = nation:make(<<"Portugal">>, [{<<"beer">>, 100}, {<<"cloth">>, 90}, {<<"wine">>, 80}]),
+    Results = ricardo:optimize([Eng, Ger, Por]),
+    ?assertEqual(#{<<"England">>  => [<<"cloth">>],
+                   <<"Germany">>  => [<<"beer">>],
+                   <<"Portugal">> => [<<"wine">>]}, Results).
+
+quadruple_case() ->
+    Eng = nation:make(<<"England">>, [{<<"beer">>, 110}, {<<"cheese">>, 100}, {<<"cloth">>, 100}, {<<"wine">>, 120}]),
+    Fra = nation:make(<<"France">>, [{<<"beer">>, 100}, {<<"cheese">>, 60}, {<<"cloth">>, 90}, {<<"wine">>, 80}]),
+    Ger = nation:make(<<"Germany">>, [{<<"beer">>, 50}, {<<"cheese">>, 80}, {<<"cloth">>, 80}, {<<"wine">>, 90}]),
+    Por = nation:make(<<"Portugal">>, [{<<"beer">>, 100}, {<<"cheese">>, 90}, {<<"cloth">>, 90}, {<<"wine">>, 80}]),
+    Results = ricardo:optimize([Eng, Fra, Ger, Por]),
+    ?assertEqual(#{<<"England">>  => [<<"cloth">>],
+                   <<"France">>   => [<<"cheese">>],
+                   <<"Germany">>  => [<<"beer">>],
+                   <<"Portugal">> => [<<"wine">>]}, Results).
 
 %%% PRIVATE FUNCTIONS
 
